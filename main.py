7bfbba3d251f0163d445866e7c9c10fa4d8748b7
Alex Krantz <alex@krantz.dev>
2020-06-06 18:30:37 -0700

    Add comments

--- main.py
+++ main.py
@@ -11,21 +11,27 @@ app = Flask(__name__)
 
 @app.route("/api/ratings", methods=["GET", "POST"])
 def rate():
+    # Handle get method
     if request.method == "GET":
+        # Ensure department parameter exists
         if request.args.get("department") is None:
             return jsonify({"status": "error", "reason": "query parameter 'department' is required"}), 400
 
+        # Construct query for all posts with matching department
         query = datastore_client.query(kind="Post")
         query.add_filter("department", "=", request.args.get("department"))
 
+        # Retrieve all posts
         entities = [entity for entity in query.fetch()]
         return jsonify({"status": "success", "data": entities})
 
+    # Validate request body by schema in `./schemas/post.json`
     try:
         POST_VALIDATOR(request.json)
     except JsonSchemaException as e:
         return jsonify({"status": "error", "reason": f"invalid json format: {e}"}), 400
 
+    # Create key and associated entity
     key = datastore_client.key("Post")
     entity = datastore.Entity(key)
     entity.update({
@@ -42,6 +48,8 @@ def rate():
         "gender": request.json['tags']['gender'],
         "age": request.json['tags']['age']
     })
+
+    # Insert into datastore
     datastore_client.put(entity)
 
     return jsonify({"status": "success"})
@@ -49,13 +57,16 @@ def rate():
 
 @app.route("/api/departments/<string:state>")
 def departments_for_state(state):
+    # Ensure state code is consistent with those in datastore
     state = state.upper()
     if len(state) != 2:
         return jsonify({"status": "error", "reason": "state code must be two characters long"}), 400
 
+    # Construct query for all departments with matching state abbreviation
     query = datastore_client.query(kind="Department")
     query.add_filter("state_code", "=", state)
 
+    # Retrieve all departments
     entities = [entity for entity in query.fetch()]
     return jsonify({"status": "success", "data": entities})
 
