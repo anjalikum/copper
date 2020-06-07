592b543a0ee4c89d38d2efd0c7ac3bbe3cc88892
yuyuan luo <luo.yuyuan@yahoo.com>
2020-06-07 04:45:59 -0400

    add ratings

--- js/search.js
+++ js/search.js
@@ -22,6 +22,20 @@ $( document ).ready(function() {
           var lis = document.getElementById("cop-info");
           lis.appendChild(newdiv);
           newdiv.appendChild(document.createTextNode(ar[i].comments));
+          newdiv.appendChild(document.createElement("br"));
+          newdiv.appendChild(document.createElement("br"));
+          newdiv.appendChild(document.createElement("br"));
+          newdiv.appendChild(document.createTextNode("Average rating " + ave(ar[i].friendliness, ar[i].difficulty, ar[i].appropriateness, ar[i].helpfulness, ar[i].nonviolence)));
+          newdiv.appendChild(document.createElement("br"));
+          newdiv.appendChild(document.createTextNode("Friendliness " + ar[i].friendliness));
+          newdiv.appendChild(document.createElement("br"));
+          newdiv.appendChild(document.createTextNode("Ease of Interaction " + ar[i].difficulty));
+          newdiv.appendChild(document.createElement("br"));
+          newdiv.appendChild(document.createTextNode("Appropriateness " + ar[i].appropriateness));
+          newdiv.appendChild(document.createElement("br"));
+          newdiv.appendChild(document.createTextNode("Helpfulness " + ar[i].helpfulness));
+          newdiv.appendChild(document.createElement("br"));
+          newdiv.appendChild(document.createTextNode("Nonviolence " + ar[i].nonviolence));
           newdiv.setAttribute("class", "badgenum");
       }
     });
@@ -29,3 +43,6 @@ $( document ).ready(function() {
   });
 });
 
+function ave (a,b,c,d,e) {
+  return (a+b+c+d+e)/5;
+}
